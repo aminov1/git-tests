ec05ca00fca777a51e513291ffc7d74b72800ded
amin.habash <amin.habash@hpe.com>
2017-01-29 11:12:35 +0200

    Commit #7

---
+++ Pull.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Staging changes..."
+git ls-files -o --exclude-standard | xargs -d '\n' git add 
+echo "Committing..."
+commitNum=$(git log master --pretty=oneline | wc -l)
+git commit -m "Commit #"$((commitNum + 1))
+echo "Removing unwanted changes..."
+git clean -df
+git checkout -- .
+echo "Pulling from remote repository..."
+git pull --rebase origin
+echo "Done!"
+read
---
+++ Push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Staging changes..."
+git ls-files -o --exclude-standard | xargs -d '\n' git add 
+echo "Committing..."
+commitNum=$(git log master --pretty=oneline | wc -l)
+git commit -m "Commit #"$((commitNum + 1))
+echo "Removing unwanted changes..."
+git clean -df
+git checkout -- .
+echo "Pushing to remote repository..."
+git push origin master
+echo "Done!"
+read
